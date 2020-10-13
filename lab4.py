cbfd73efc425f924edb9f85c3653ffdd6ef196e7
Lenin Valdivia <59747577+Oqqay@users.noreply.github.com>
2020-10-13 15:55:38 -0700

    final code (?)

--- lab4.py
+++ lab4.py
@@ -8,13 +8,13 @@ def main():
     square = solvableSquare()
     solved = solvedSquare()
     movesMade = 0
-    for row in square:
+    for row in square:  # displays generated square
         print("")
         for col in row:
             if col == "\N{WHITE SQUARE}":
-                print("\N{WHITE SQUARE} ", end = "")
+                print("\N{WHITE SQUARE} ", end="")
             else:
-                print("\N{BLACK SQUARE} ", end = "")
+                print("\N{BLACK SQUARE} ", end="")
     while square != solved:
         print("")
         row = int(input("Choose a row number(0-4): "))
@@ -24,9 +24,9 @@ def main():
             print("")
             for col in row:
                 if col == "\N{WHITE SQUARE}":
-                    print("\N{WHITE SQUARE} ", end = "")
+                    print("\N{WHITE SQUARE} ", end="")
                 else:
-                    print("\N{BLACK SQUARE} ", end = "")
+                    print("\N{BLACK SQUARE} ", end="")
         movesMade += 1
         print("")
         print("Moves so far: " + str(movesMade))
@@ -38,6 +38,12 @@ def main():
         print("Oh well, hope to see you again soon! Goodbye!")
         return movesMade
 
+# The unicodes and strings for the white and black blocks when printed
+# from a list are essentially the same thing. We could replace the unicodes
+# below with the strings '□' and '■', then just make sure to print out
+# the unicodes instead in the main function. But for consistency's sake,
+# we're gonna keep using the unicode bellow here aswell.
+
 
 def puzzle(inRow, inColl, square):
     selection = square[inRow][inColl]
@@ -47,7 +53,7 @@ def puzzle(inRow, inColl, square):
     elif selection == "\N{BLACK SQUARE}":
         square[inRow][inColl] = "\N{WHITE SQUARE}"
 
-    if inRow == 0:
+    if inRow == 0:  # Edge cases
         if inColl == 0:
             square[inRow][inColl + 1] = opposite(square[inRow][inColl + 1])
             square[inRow + 1][inColl] = opposite(square[inRow + 1][inColl])
@@ -91,7 +97,7 @@ def puzzle(inRow, inColl, square):
 
 def solvableSquare():
     blank = solvedSquare()
-    for _ in range(random.randint(15, 50)):
+    for _ in range(random.randint(15, 50)):  # randomize for random # of times
         row = random.randrange(0, 5)
         col = random.randrange(0, 5)
         blank = puzzle(row, col, blank)
